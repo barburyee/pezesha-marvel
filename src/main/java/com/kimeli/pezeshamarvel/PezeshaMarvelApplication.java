f0658da8647c71c5290a94ce87fefaf0f627c643
Kimeli <jobkimeli2016@gmail.com>
2022-10-12 18:55:05 +0300

    Initial commit

---
+++ src/main/java/com/kimeli/pezeshamarvel/PezeshaMarvelApplication.java
@@ -0,0 +1,13 @@
+package com.kimeli.pezeshamarvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PezeshaMarvelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PezeshaMarvelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimeli/pezeshamarvel/PezeshaMarvelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimeli.pezeshamarvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PezeshaMarvelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
